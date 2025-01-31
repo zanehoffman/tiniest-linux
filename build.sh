12f9713126829ea51b6d110d520a034f2bbdd107
Michael <mic.ric.tor@gmail.com>
2025-01-31 23:33:23 +0000

    Down to 166 bytes

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-as --64 sys.S && ld -O1 -n -nmagic -z max-page-size=1 a.out -o init && llvm-strip-13 --strip-all --strip-sections init && wc -c init 
+as --64 sys.S -o sys.o && ld --oformat binary -o init sys.o && wc init
